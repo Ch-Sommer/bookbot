8ad55b374e88e09c3137c23d7437c13465b75d6f
CSO-Tinkering <sommer.christian@sunrise.ch>
2024-04-15 08:05:06 +0200

    changed main.py to print content of frankenstein.txt

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+def main():
+    try:
+        frankenstein_string = "..."
+        with open('books/frankenstein.txt', 'r') as f:
+            frankenstein_string = f.read()
+            print(frankenstein_string)
+
+    except FileNotFoundError:
+        print(f"File '{'books/frankenstein.txt'}' not found.")
+    except Exception as e:
+        print(f"An error occured: {e}")
+main()
