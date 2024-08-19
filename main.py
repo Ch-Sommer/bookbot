4f13be09cd0183964f0ea0b1b8dca7de4b328283
CSO-Tinkering <sommer.christian@sunrise.ch>
2024-08-19 16:16:45 +0200

    Minus the The at the end

--- main.py
+++ main.py
@@ -69,5 +69,5 @@ def main():
             continue
         print(f"The '{item['char']}' character was found {item['num']} times")
 
-    print("--- The end of the report ---")
+    print("--- End of the report ---")
 main()
