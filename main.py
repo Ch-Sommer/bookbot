f8776f8b58d2a2cb564aa48d4ddbd0080fb1100d
CSO-Tinkering <sommer.christian@sunrise.ch>
2024-09-22 03:01:58 +0200

    main method hello world again

--- main.py
+++ main.py
@@ -54,21 +54,21 @@ def chars_dict_to_sorted_list(num_chars_dict):
 
 def main():
     print("hello world")
-    source='books/frankenstein.txt'
-    text=get_text_from_file(source)
-    words=word_count(text)
-    count_letters=count_letter(text)
-    #print(f"{text} \nThere are {words} words in the document {source}.")
-    #print(f"{count_letters}")
-    chars_sorted_list = chars_dict_to_sorted_list(count_letters)
-    print(f"--- Start of the report for {source} ---")
-    print(f"{words} words found in the document")
-    print()
+    #source='books/frankenstein.txt'
+    #text=get_text_from_file(source)
+    #words=word_count(text)
+    #count_letters=count_letter(text)
+    ##print(f"{text} \nThere are {words} words in the document {source}.")
+    ##print(f"{count_letters}")
+    #chars_sorted_list = chars_dict_to_sorted_list(count_letters)
+    #print(f"--- Start of the report for {source} ---")
+    #print(f"{words} words found in the document")
+    #print()
 
-    for item in chars_sorted_list:
-        if not item["char"].isalpha():
-            continue
-        print(f"The '{item['char']}' character was found {item['num']} times")
+    #for item in chars_sorted_list:
+    #    if not item["char"].isalpha():
+    #        continue
+    #    print(f"The '{item['char']}' character was found {item['num']} times")
 
-    print("--- End of the report ---")
+    #print("--- End of the report ---")
 main()
